6e3f2fd28e5255937502c0931e362a4d57212095
McRAHMN <97932290+McRAHMN@users.noreply.github.com>
2022-03-08 09:56:13 +0700

    Add files via upload

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="row">
-    <div class="col-md-12">
-      <h1>Contact</h1>
-      <peta />
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>contact</h1>
+        <peta />
+      </div>
+      <div>
+        <formcontact />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import peta from "../components/peta.vue";
-
+import formcontact from "../components/formcontact.vue";
 export default {
   components: {
     peta,
+    formcontact,
   },
 };
 </script>
